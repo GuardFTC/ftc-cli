c523ea16adebc90d9439e2fb33b417c25c222662
fengtiecheng <17615007230@163.com>
2025-11-03 17:01:41 +0800

    feat(sql): 添加 ads_bi_af_retention_d_i 表的字段映射配置

--- cmd/sql/config.go
+++ cmd/sql/config.go
@@ -20,4 +20,14 @@ var tableColumnMap = map[string][]string{
 		"rev10", "rev11", "rev12", "rev13", "rev14", "rev15", "rev30", "rev60", "rev90",
 		"rev120", "rev150", "rev180", "rev210", "rev240", "rev270", "rev300", "rev330", "rev360",
 	},
+	"ads_bi_af_retention_d_i": {
+		"game_id", "data_type", "dt", "platform", "country",
+		"media_source", "ad_material", "campaign", "campaign_id",
+		"cost", "click", "impression", "nu",
+		"ua1", "ua2", "ua3", "ua4", "ua5", "ua6", "ua7", "ua8", "ua9", "ua10",
+		"ua11", "ua12", "ua13", "ua14", "ua15",
+		"ua30", "ua60", "ua90", "ua120", "ua150",
+		"ua180", "ua210", "ua240", "ua270",
+		"ua300", "ua330", "ua360",
+	},
 }
